30d46de6829f4d6e2df0376ce8dc79c5ba7e5a23
danijmj <danielmorales@gnoss.com>
2024-07-12 17:19:31 +0200

    Changes in the routes, added the route to new planet and modify the edit a new planet route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import NewPlanet from '@/views/NewPlanet.vue'
 
 
 export const RouteNames = {
   HOME: 'home',
   PLANET: 'planet',
   EDITPLANET: 'edit-planet',
+  PLANETNEW: 'planet-new',
 }
 
 const router = createRouter({
@@ -17,24 +19,31 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/planet/:id?',
+      path: '/planet/:id',
       name: RouteNames.PLANET,
       meta: {
         title: 'View Planet',
       },
       component: () => import('@/views/ViewPlanet.vue'),
       props: true,
-      children: [
-        {
-          path: 'edit',
-          name: RouteNames.EDITPLANET,
-          meta: {
-            title: 'Edit Planet',
-          },
-          component: import('@/views/ViewPlanet.vue'),
-          props: true,
-        },
-      ]
+    },
+    
+    {
+      path: '/planet/edit/:id',
+      name: RouteNames.EDITPLANET,
+      meta: {
+        title: 'Edit Planet',
+      },
+      component: () => import('@/views/NewPlanet.vue'),
+      props: true,
+    },
+    {
+      path: '/planet/new',
+      name: RouteNames.PLANETNEW,
+      meta: {
+        title: 'New Planet',
+      },
+      component: () => import('@/views/NewPlanet.vue')
     }
   ]
 })
